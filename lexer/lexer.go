87c250f95c8f4e183eb521d69d39ee6b4cbef1b6
Ryosuke Sato <rskjtwp@gmail.com>
2018-07-02 19:35:46 +0900

    Add new Lexer

---
+++ lexer/lexer.go
@@ -0,0 +1,60 @@
+package lexer
+
+import "github.com/ryosan-470/monkey-lang/token"
+
+// Lexer は 字句解析を行うための構造体
+type Lexer struct {
+	input        string
+	position     int  // 入力における現在位置
+	readPosition int  // 入力の次の文字位置。先読み
+	ch           byte // 現在の文字
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+// TODO: ASCII以外 (例えばUTF-8) に対応させる
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0 // ASCIIのNUL文字に対応
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Type = token.EOF
+		tok.Literal = ""
+	}
+
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
