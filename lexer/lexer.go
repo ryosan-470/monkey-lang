4018b86ff2f6aa34d8b8066197f372ce070e6b33
Ryosuke Sato <rskjtwp@gmail.com>
2018-07-02 20:01:54 +0900

    整数列を読み込めるように

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -35,6 +35,15 @@ func (l *Lexer) readIdentifier() string {
 	return l.input[position:l.position]
 }
 
+// TODO: 整数以外 (小数とか) を読み込めるようにする
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
 // skipWhitespace により、不要な文字列を飛ばす
 func (l *Lexer) skipWhitespace() {
 	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
@@ -72,6 +81,10 @@ func (l *Lexer) NextToken() token.Token {
 			tok.Literal = l.readIdentifier()
 			tok.Type = token.LookupIdent(tok.Literal)
 			return tok // すでに readIdentifier() で l.position が先に進んでいるためここでもどす
+		} else if isDigit(l.ch) {
+			tok.Literal = l.readNumber()
+			tok.Type = token.INT
+			return tok
 		} else {
 			tok = newToken(token.ILLEGAL, l.ch)
 		}
@@ -87,4 +100,8 @@ func newToken(tokenType token.TokenType, ch byte) token.Token {
 
 func isLetter(ch byte) bool {
 	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
 }
